f5b0a64aa9b22fbbd9faab6e65a5c8501db7610e
Ben Evans <ben@bensbit.co.uk>
2012-12-26 16:24:22 +0000

    highlight doc data

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ hbs.registerHelper('doc_table', function(doc) {
   rows.push('<th>_id</th><th>' + doc._id + '</th>');
   for (var key in doc) {
     if(key !== '_id')
-      rows.push('<th>' + key + '</th><td>' + JSON.stringify(doc[key], null, 2) + '</td>');
+      rows.push('<th>' + key + '</th><td><code class="prettyprint languague-json">' + JSON.stringify(doc[key], null, 2) + '</code></td>');
   }
   return ['<table class="table table=hover table-bordered table-striped">',
   '<tr>',
