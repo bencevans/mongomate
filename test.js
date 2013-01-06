d6a676205d71c3dd87bc168cb1542ca8b1cf4587
Ben Evans <ben@bensbit.co.uk>
2013-01-06 22:58:39 +0000

    need new method to test cli

--- test.js
+++ test.js
@@ -4,51 +4,23 @@ var MongoMate = require('./');
 var exec = require('child_process').exec;
 var http = require('http');
 
-
-describe('standalone', function() {
-
-  it('starts', function(done) {
-
-    var child = exec('./bin/mongomate');
-
-    setTimeout(function() {
-      http.get('http://localhost:8080', function(res) {
-        assert.equal(200, res.statusCode);
-        child.kill();
-        done();
-      });
-    }, 1000);
-
-  });
-
-  it('starts on specified port', function(done) {
-
-    var child = exec('./bin/mongomate', {
-      env: {
-        PORT:'8081'
-      }
-    });
-
-    setTimeout(function() {
-      http.get('http://localhost:8081', function(res) {
-        assert.equal(200, res.statusCode);
-        child.kill();
-        done();
-      });
-    }, 1000);
-
-  });
-
+describe('mongomate', function() {
   it('displays error when not connected');
   it('exports version', function() {
     assert.equal(typeof MongoMate.version, 'string');
   });
-
 });
 
 describe('mounted app', function() {
+  it('exports app', function() {
+    assert.ok(MongoMate().listen);
+  });
+});
 
-  it('exports app');
+describe('cli', function() {
+
+  it('starts');
+  it('starts on specified port');
 
 });
 
